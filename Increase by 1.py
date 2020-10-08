145186b9604baa5e2f8f5bcedb3dd272defa88e4
themoat <aditya@ayemalls.com>
2020-10-08 12:03:16 -0700

    Create Increase by 1.py

---
+++ Increase by 1.py
@@ -0,0 +1,20 @@
+from math import ceil, floor
+t = int(input())
+
+# for i in range(0,t):
+#     n = int(input())
+#     if n==1:
+#         print("0")
+#     else:
+#         print(ceil(n**0.5-1)*2)
+#
+
+
+for i in range(0,t):
+    n = int(input())
+    a=floor(n**0.5)
+    res = a - 1 + floor(n/a) - 1
+    if n%(floor(n/a)*a)==0:
+        print(res)
+    else:
+        print(res+1)
